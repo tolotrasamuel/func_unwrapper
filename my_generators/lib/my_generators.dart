996dd6b1b65a88d5e6b03fbaf42dc165438f49c5
Tolotra <samuel.randriakotonjanahary@mcb.mu>
2021-05-26 16:51:48 +0400

    Removed unused combine builder

--- my_generators/lib/my_generators.dart
+++ my_generators/lib/my_generators.dart
@@ -2,15 +2,7 @@ import 'package:build/build.dart';
 import 'package:my_generators/src/function_unwrap.dart';
 import 'package:source_gen/source_gen.dart';
 
-import 'src/class_extras_generator.dart';
 import 'src/info_generator.dart';
-import 'src/serialize_generator.dart';
-
-Builder classExtrasGeneratorBuilder(BuilderOptions options) =>
-    SharedPartBuilder([ClassExtrasGenerator()], 'fields');
-
-Builder serializeGeneratorBuilder(BuilderOptions options) =>
-    SharedPartBuilder([SerializeGenerator()], 'serialize');
 
 Builder functionUnwrapBuild(BuilderOptions options) => LibraryBuilder(
       FunctionUnwrap(),
